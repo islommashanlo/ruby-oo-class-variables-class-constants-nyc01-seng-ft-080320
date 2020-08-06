5fb0d6d108ab1af79580f6eb9d28aab223caa2d8
Islom Mashanlo <islom.mashanlo@gmail.com>
2020-08-06 01:29:19 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -2,7 +2,7 @@ class Book
   attr_accessor :author, :page_count, :genre
   attr_reader :title
   GENRES = []
-  def initialize(genre)
+  def initialize(title, author, genre, page_count)
     @title = title
     @genre = genre
     @author = author
